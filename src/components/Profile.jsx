d0be558384e7e1c693483a1b20c7ca7e156052d5
den777kaz <52310435+den777kaz@users.noreply.github.com>
2020-07-07 21:05:51 +0200

    start

---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://specials-images.forbesimg.com/imageserve/5ebed2cc142a910006474ad8/960x0.jpg?fit=scale" alt=""/>
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        New Posts
+      </div>
+      <div>post1</div>
+      <div>post2</div>
+    </div>
+  )
+}
+
+
+export default Profile;
