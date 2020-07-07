2386ca8860caefbc2fbe96a372e90a28310ccc5e
den777kaz <52310435+den777kaz@users.noreply.github.com>
2020-07-07 21:10:47 +0200

    merge

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://e7.pngegg.com/pngimages/723/887/png-clipart-computer-icons-x-mark-check-mark-red-x-miscellaneous-text-thumbnail.png" alt="logo"/>
+    </header>
+  )
+}
+
+
+export default Header;
