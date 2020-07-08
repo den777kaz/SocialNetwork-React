bae5fe69a17d371eb94c748e99cffdcaddb6e5f0
den777kaz <52310435+den777kaz@users.noreply.github.com>
2020-07-08 20:50:52 +0200

    active + new component

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -5,7 +5,22 @@ import style from './Dialogs.module.css'
 const Dialogs = () => {
   return (
    <div className={style.dialogs}>
-     dialogs content
+     <div className={style.dialogs_items}>
+       <div className={`${style.dialog} ${style.active}`}>
+         <span className={`${style.status} ${style.active}`}></span>Denis
+       </div>
+       <div className={style.dialog}>
+         <span className={style.status}></span>John
+       </div>
+       <div className={style.dialog}>
+         <span className={style.status}></span>Dimi4
+       </div>
+     </div>
+     <div className={style.messages}>
+       <div className={style.message}> messasdsdsscsdcscsdcscscscsdcsdcsdge 1</div>
+       <div className={style.message}> message 2</div>
+       <div className={style.message}> message 3</div>
+     </div>
    </div>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './Header.module.css';
 
+
 const Header = () => {
   return (
     <header className={style.header}>
