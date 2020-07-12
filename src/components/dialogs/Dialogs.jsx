c505f371ed814f0373954e87aefec891a7000691
den777kaz <52310435+den777kaz@users.noreply.github.com>
2020-07-12 12:28:36 +0200

    22. Уроки React JS (Route exact) - react курсы бесплатно

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import style from './Dialogs.module.css'
+import style from './Dialogs.module.css';
+import {NavLink}  from 'react-router-dom';
 
 
 const Dialogs = () => {
@@ -7,13 +8,16 @@ const Dialogs = () => {
    <div className={style.dialogs}>
      <div className={style.dialogs_items}>
        <div className={`${style.dialog} ${style.active}`}>
-         <span className={`${style.status} ${style.active}`}></span>Denis
+         <span className={`${style.status} ${style.active}`}></span>
+         <NavLink to="/dialogs/1" >Denis</NavLink>
        </div>
        <div className={style.dialog}>
-         <span className={style.status}></span>John
+         <span className={style.status}></span>
+         <NavLink to="/dialogs/2" >John</NavLink>
        </div>
        <div className={style.dialog}>
-         <span className={style.status}></span>Dimi4
+         <span className={style.status}></span>
+         <NavLink to="/dialogs/3" >Dimi4</NavLink>
        </div>
      </div>
      <div className={style.messages}>
