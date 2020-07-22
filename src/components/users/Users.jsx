416c2568e76fd9de4b1adfbdd9ec97cc7f080f50
den777kaz <52310435+den777kaz@users.noreply.github.com>
2020-07-22 15:14:19 +0200

    users= functional component _-> class component   + lifecycle

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -4,26 +4,24 @@ import * as axios from "axios";
 import userPhoto from "./../../assets/images/user.jpg"
 
 class Users extends React.Component {
-constructor(props) {
-  super(props);
 
+componentDidMount(){
   axios.get("https://social-network.samuraijs.com/api/1.0/users")
     .then(response => {
-      props.setUsers(response.data.items);
+      this.props.setUsers(response.data.items);
     })
 }
-
 render () {
   return (
     <div className={style.users}>
       {
         this.props.users.map(u =>
 
-          <div key={u.id}>
+          <div className={style.item} key={u.id}>
 
             <span>
               <div>
-                <img src={u.photos.small != null ? u.phos.small : userPhoto } alt="photo"/>
+                <img src={u.photos.small != null ? u.photos.small : userPhoto } />
               </div>
               <div>
                 { u.followed
